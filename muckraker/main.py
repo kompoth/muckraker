9a1ef6e258a197b002bbd4bbcb50972fceb8f81e
Vasily Negrebetskiy <kmiziz.com@gmail.com>
2024-04-01 22:45:57 +0400

    Minor

--- muckraker/main.py
+++ muckraker/main.py
@@ -16,14 +16,20 @@ app.add_middleware(
 )
 
 
-@app.post("/issue")
+@app.post("/issue/")
 async def create_issue(issue: Issue):
     with tempfile.TemporaryDirectory() as tempdir:
+        # Render PDF and save it in the tempdir
         pdf_tmp_path = Path(tempdir) / "out.pdf"
         render_issue(issue.config.model_dump(), issue.body, pdf_tmp_path)
+
+        # Save PDF to the buffer
         with open(pdf_tmp_path, "rb") as fd:
             buf = BytesIO(fd.read())
+
+    # Get it from the buffer
     pdf_bytes = buf.getvalue()
     buf.close()
+
     headers = {'Content-Disposition': 'attachment; filename="out.pdf"'}
     return Response(pdf_bytes, headers=headers, media_type='application/pdf')
